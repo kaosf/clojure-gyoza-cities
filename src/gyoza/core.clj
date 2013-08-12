821938a32613948a7c5a67fdfa7cbdb625d8718c
ka <ka.kaosf@gmail.com>
2013-08-13 01:58:00 +0900

    modify gyoza/core.clj

--- src/gyoza/core.clj
+++ src/gyoza/core.clj
@@ -1,6 +1,17 @@
-(ns gyoza.core)
+(ns gyoza.core
+  (:require
+    [clojure.java.io :as io]
+    [tnoda.xls :as xls]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def url "http://www.e-stat.go.jp/SG1/estat/GL02020101.do?method=xlsDownload&fileId=000006351598&releaseCount=1")
+
+(defn gyoza-cities
+  []
+  (->>
+    (-> url io/input-stream (xls/read-sheet "2"))
+    (apply map list)
+    (filter #(some #{"Saitama-shi" "Jiao-zi"} %))
+    (apply map list)
+    (filter (comp number? second))
+    (sort-by (comp - second))
+    (take 5)))
